bdf801e9e5022ba7fc73dfa4fc93974b5c081429
Andrew Pearson <andrewtpearson94@gmail.com>
2023-04-21 20:00:45 +0100

    Added client-side service to make offers of sessions; need to write components in client which will use this service.

--- MatronClient/services/sessions-service.tsx
+++ MatronClient/services/sessions-service.tsx
@@ -7,3 +7,26 @@ import { BASEURL } from "./services-config";
 // respond to a session offer
 // confirm you are happy with a counteroffer
 
+export async function createSessionFromParent (sessionDetails) {
+  let body = JSON.stringify({
+    parent: sessionDetails.username,
+    ID: sessionDetails.username,
+    location: sessionDetails.location,
+    startTime: sessionDetails.startTime,
+    endTime: sessionDetails.endTime,
+    carer: null,
+    children: sessionDetails.children,
+    carerOptions: []
+  })
+  let result = await fetch(BASEURL + '/createParentOffer', {
+    method: 'POST',
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: body
+  }).catch(function (error) {
+    console.log('there was an error:', error);
+  });
+  let newSession = await result.json();
+  console.log(newSession, 'SS');
+}
